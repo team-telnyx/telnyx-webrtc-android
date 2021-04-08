19499f17e3e0a9cfd12e91ecc32edc890bf9b2c8
Oliver Zimmerman <oezimmerman@gmail.com>
2021-04-08 17:38:05 +0100

    Update setup_env.sh

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -11,6 +11,6 @@ echo "User: $user";
 echo "password: $password";
 echo "Token Name: $token";
 
-sed -i '' 's/<SIP_USER>/'"$user"'/g' ./app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
-sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' ./app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
-sed -i '' 's/<SIP_TOKEN>/'"$token"'/g' ./app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_USER>/'"$user"'/g' app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_TOKEN>/'"$token"'/g' app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
