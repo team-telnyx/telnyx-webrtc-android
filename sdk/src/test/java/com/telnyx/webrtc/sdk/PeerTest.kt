22880a6ade287ff3fea45f6d27b968d2a44e074f
Oliver Zimmerman <oezimmerman@gmail.com>
2021-04-19 17:24:32 +0100

    Remove unnecessary comments and code

--- sdk/src/test/java/com/telnyx/webrtc/sdk/PeerTest.kt
+++ sdk/src/test/java/com/telnyx/webrtc/sdk/PeerTest.kt
@@ -21,32 +21,10 @@ import org.webrtc.PeerConnection
 import org.webrtc.PeerConnectionFactory
 
 
-//@ExtendWith(InstantExecutorExtension::class, CoroutinesTestExtension::class)
 class PeerTest : BaseTest() {
-
-    @Spy
-    private var mockContext: Context = Mockito.spy(Context::class.java)
-
-    @MockK
-    private lateinit var observer: PeerConnection.Observer
 
     @MockK
     private var options = Mockito.mock(PeerConnectionFactory.InitializationOptions::class.java)
-
-    @MockK
-    private var peerConnection = Mockito.mock(PeerConnection::class.java)
-
-    @MockK
-    private var peer = Mockito.mock(Peer::class.java)
-
-   // @Spy
-    private lateinit var client: TelnyxClient
-
-   // @Spy
-    private lateinit var socket: TxSocket
-
-    @Spy
-    private var audioManager: AudioManager = Mockito.spy(AudioManager::class.java)
 
     @BeforeEach
     fun setup() {
@@ -55,24 +33,5 @@ class PeerTest : BaseTest() {
         mockkStatic(PeerConnection::class)
         every {PeerConnectionFactory.initialize(options)} just runs
     }
-
-    /*@Test
-    fun `instantiate peer connection without error`() {
-
-        @Spy
-        socket = Mockito.spy(TxSocket(
-            host_address = "rtc.telnyx.com",
-            port = 14938,
-        ))
-
-
-        @Spy
-        client = Mockito.spy(TelnyxClient(mockContext, socket))
-
-        val newCall = Call(mockContext, client, socket, "123", audioManager)
-        newCall.newInvite("Test", "00000", "00000", "Test State")
-        Mockito.verify(peer, Mockito.times(1)).startLocalAudioCapture()
-
-    }*/
 
 }
