a5a3b0fff2f2bfed554835d9d29606dd1442f1e7
Oliver Zimmerman <oezimmerman@gmail.com>
2021-04-23 10:59:41 +0100

    Add sample process message logic

--- sdk/src/main/java/com/telnyx/webrtc/sdk/utilities/fcm/TelnyxFcm.kt
+++ sdk/src/main/java/com/telnyx/webrtc/sdk/utilities/fcm/TelnyxFcm.kt
@@ -2,10 +2,31 @@ package com.telnyx.webrtc.sdk.utilities.fcm
 
 import android.content.Context
 import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
 
 object TelnyxFcm {
-    fun processPushMessage(context: Context, message: RemoteMessage) {
-        //ToDo
+    fun processPushMessage(context: Context, remoteMessage: RemoteMessage) {
+        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
+        Timber.d( " Message From: ${remoteMessage.from}")
+
+        // Check if message contains a data payload.
+        if (remoteMessage.data.isNotEmpty()) {
+            Timber.d("Message data payload: ${remoteMessage.data}")
+
+            if (/* Check if data needs to be processed by long running job */ true) {
+                // For long-running tasks (10 seconds or more) use WorkManager.
+               // scheduleJob()
+            } else {
+                // Handle message within 10 seconds
+              //  handleNow()
+            }
+        }
+
+        // Check if message contains a notification payload.
+        remoteMessage.notification?.let {
+            Timber.d("Message Notification Body: ${it.body}")
+        }
+
     }
 
     fun sendRegistrationToServer(context: Context, token: String) {
