904a288224e1bb0a44aa1522f1b217ddd1bd5d2a
Oliver Zimmerman <oezimmerman@gmail.com>
2021-04-26 14:33:31 +0100

    Implemented ability to unregister

--- sdk/src/main/java/com/telnyx/webrtc/sdk/utilities/fcm/TelnyxFcm.kt
+++ sdk/src/main/java/com/telnyx/webrtc/sdk/utilities/fcm/TelnyxFcm.kt
@@ -36,6 +36,28 @@ object TelnyxFcm {
                     Timber.d("FCM Registration to Telnyx Notification Service failed.")
             }
         })
+    }
 
+    fun unregisterDevice(context: Context, token: String) {
+        val apiService: RetrofitAPIInterface?
+        apiService = ApiUtils.apiService
+
+        val fcmDeviceId = token.split(":").first()
+
+        apiService.registrationPost("unregister", "android", fcmDeviceId).enqueue(object :
+            Callback<FcmRegistrationResponse> {
+            override fun onResponse(call: Call<FcmRegistrationResponse>, response: Response<FcmRegistrationResponse>) {
+                if (response.isSuccessful) {
+
+                    Timber.i("post unregister to API %s", response.body().toString())
+                    Timber.i("post status to API %s", response.code().toString())
+                    Timber.i( "post msg to API %s", response.body()!!.message)
+
+                }
+            }
+            override fun onFailure(call: Call<FcmRegistrationResponse>, t: Throwable) {
+                Timber.d("FCM unregister request to Telnyx Notification Service failed.")
+            }
+        })
     }
 }
