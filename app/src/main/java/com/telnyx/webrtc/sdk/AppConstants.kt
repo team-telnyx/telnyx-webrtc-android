f25a1c17fc2ccc25fdf7cbe5cf07473dcd05210a
Oliver Zimmerman <oezimmerman@gmail.com>
2021-05-05 12:19:31 +0100

    [WEBRTC-522] Adjust mocks for automated tests

--- app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
+++ app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
@@ -6,6 +6,6 @@ package com.telnyx.webrtc.sdk
 
 const val MOCK_USERNAME = "<SIP_USER>"
 const val MOCK_PASSWORD = "<SIP_PASSWORD>"
-const val MOCK_CALLER_NAME = "<TEST_NAME>"
-const val MOCK_CALLER_NUMBER = "<TEST_NUMBER>"
+const val MOCK_CALLER_NAME = "NAME"
+const val MOCK_CALLER_NUMBER = "000000000"
 const val MOCK_DESTINATION_NUMBER = "+35315227159"
