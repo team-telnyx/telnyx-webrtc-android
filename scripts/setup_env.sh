0b715671daf62da443aaf5b7778262c050c61996
Oliver Zimmerman <oezimmerman@gmail.com>
2021-07-29 12:54:09 +0100

    [WEBRTC-644] Fix shell script file path reference

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -9,12 +9,12 @@ done
 echo "User: $user";
 echo "password: $password";
 
-sed -i '' 's/<SIP_USER>/'"$user"'/g' app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
-sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_USER>/'"$user"'/g' ../app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' ../app/src/androidTest/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
 
-sed -i '' 's/<SIP_USER>/'"$user"'/g' app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
-sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
+sed -i '' 's/<SIP_USER>/'"$user"'/g' ../app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
+sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' ../app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
 
-sed -i '' 's/<SIP_USER>/'"$user"'/g' telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
-sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_USER>/'"$user"'/g' ../telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
+sed -i '' 's/<SIP_PASSWORD>/'"$password"'/g' ../telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/testhelpers/TestConstants.kt
 
