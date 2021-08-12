2f8feda8069e42a0935c0b26302bfe5432c36ee6
Oliver Zimmerman <oezimmerman@gmail.com>
2021-08-12 16:44:44 +0100

    [WEBRTC-656] Fix wait time for instrument test

--- app/src/androidTest/java/com/telnyx/webrtc/sdk/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/telnyx/webrtc/sdk/ui/MainActivityTest.kt
@@ -167,11 +167,11 @@ class MainActivityTest : BaseUITest() {
             onView(withId(R.id.sip_username_id)).perform(setTextInTextView(context.resources.getString(R.string.mock_username)))
             Thread.sleep(1500)
             onView(withId(R.id.sip_password_id)).perform(setTextInTextView(context.resources.getString(R.string.mock_password)))
-            Thread.sleep(6500)
+            Thread.sleep(1500)
             onView(withId(R.id.connect_button_id))
                 .perform(closeSoftKeyboard())
                 .perform(click())
-            Thread.sleep(3000)
+            Thread.sleep(6500)
 
             onView(withId(R.id.call_button_id))
                 .perform(closeSoftKeyboard())
