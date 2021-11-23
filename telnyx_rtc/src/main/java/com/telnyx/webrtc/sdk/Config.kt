9d342707c3792869a6113af65eb2437cf966143a
Oliver Zimmerman <oezimmerman@gmail.com>
2021-11-23 11:00:39 +0000

    [WEBRTC-716] Change default Telnyx Port

--- telnyx_rtc/src/main/java/com/telnyx/webrtc/sdk/Config.kt
+++ telnyx_rtc/src/main/java/com/telnyx/webrtc/sdk/Config.kt
@@ -7,7 +7,7 @@ package com.telnyx.webrtc.sdk
 internal object Config {
         const val TELNYX_PROD_HOST_ADDRESS = "rtc.telnyx.com"
         const val TELNYX_DEV_HOST_ADDRESS = "rtcdev.telnyx.com"
-        const val TELNYX_PORT = 14938
+        const val TELNYX_PORT = 443
         const val DEFAULT_TURN = "turn:turn.telnyx.com:3478?transport=tcp"
         const val DEFAULT_STUN = "stun:stun.telnyx.com:3843"
         var USERNAME = "testuser"
