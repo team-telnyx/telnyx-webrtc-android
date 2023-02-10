4b5ae6875582b45ca9df667d301f72f9c1f10176
RakshakGaind0910 <rakshakgaind0910@gmail.com>
2023-02-10 16:29:44 +0530

    Disconnect callback issue fixed added notification permission for android 13

--- telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/socket/TxSocketTest.kt
+++ telnyx_rtc/src/test/java/com/telnyx/webrtc/sdk/socket/TxSocketTest.kt
@@ -131,6 +131,10 @@ class TxSocketTest : BaseTest() {
         // Sleep to give time to connect
         Thread.sleep(6000)
         verify(client, times(1)).onConnectionEstablished()
+        client.pingPong()
+        Thread.sleep(30000)
+        verify(client, times(2)).pingPong()
+        socket.isPing shouldBe true
     }
 
     @Test
@@ -169,10 +173,10 @@ class TxSocketTest : BaseTest() {
         // Sleep to give time to connect
         Thread.sleep(6000)
         verify(client, times(1)).onConnectionEstablished()
-        client.pingPong()
+       /* client.pingPong()
         Thread.sleep(30000)
         verify(client, times(2)).pingPong()
-        socket.isPing shouldBe true
+        socket.isPing shouldBe true*/
     }
 
     @Test
