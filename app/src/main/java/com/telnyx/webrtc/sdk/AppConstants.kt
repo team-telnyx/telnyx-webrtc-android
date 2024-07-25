c4c1ed0f7da2d41b51f1485fd533d95a5b00999c
Isaac <134492608+isaacakakpo1@users.noreply.github.com>
2024-07-25 02:35:09 +0000

    Push Notifications Update

--- app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
+++ app/src/main/java/com/telnyx/webrtc/sdk/AppConstants.kt
@@ -4,8 +4,8 @@
 
 package com.telnyx.webrtc.sdk
 
-const val MOCK_USERNAME = "isaac97850"
-const val MOCK_PASSWORD = "DNKNZcF6"
+const val MOCK_USERNAME = "<SIP_USER>"
+const val MOCK_PASSWORD = "<SIP_PASSWORD>"
 const val MOCK_CALLER_NAME = "NAME"
 const val MOCK_CALLER_NUMBER = "000000000"
 const val MOCK_DESTINATION_NUMBER = "+35315227159"
