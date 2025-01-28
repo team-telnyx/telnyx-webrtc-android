d7f35fc1e78dd37230ad0910d5f414abb892522b
isaacakakpo1 <134492608+isaacakakpo1@users.noreply.github.com>
2025-01-28 13:27:16 +0100

    Fix AGP Version and Gradle Conflicts

--- xmlapp/src/main/java/org/telnyx/webrtc/xmlapp/login/LoginBottomSheetFragment.kt
+++ xmlapp/src/main/java/org/telnyx/webrtc/xmlapp/login/LoginBottomSheetFragment.kt
@@ -1,11 +1,12 @@
-package com.telnyx.webrtc.sdk.ui.redesign
+package org.telnyx.webrtc.xmlapp.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.telnyx.webrtc.sdk.R
+import androidx.fragment.app.Fragment
+import org.telnyx.webrtc.xmlapp.R
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -14,10 +15,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginBottomSheerFragment.newInstance] factory method to
+ * Use the [LoginBottomSheetFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginBottomSheerFragment : Fragment() {
+class LoginBottomSheetFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +36,7 @@ class LoginBottomSheerFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_bottom_sheer, container, false)
+        return inflater.inflate(R.layout.fragment_login_bottom_sheet, container, false)
     }
 
     companion object {
@@ -50,7 +51,7 @@ class LoginBottomSheerFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginBottomSheerFragment().apply {
+            LoginBottomSheetFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- xmlapp/src/main/java/org/telnyx/webrtc/xmlapp/login/LoginFragment.kt
+++ xmlapp/src/main/java/org/telnyx/webrtc/xmlapp/login/LoginFragment.kt
@@ -1,23 +1,24 @@
-package com.telnyx.webrtc.sdk.ui.redesign
+package org.telnyx.webrtc.xmlapp.login
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.telnyx.webrtc.sdk.R
+import androidx.fragment.app.Fragment
+import org.telnyx.webrtc.xmlapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
+
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [LoginFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class LoginFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +36,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
     companion object {
@@ -45,12 +46,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment LoginFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            LoginFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
