7ffa7c51eb0654e1bf3f98d58396f6c95d75ddcf
wojciechowskiradek <radoslaw@telnyx.com>
2025-02-03 13:03:27 +0100

    Feature: login functionality in common module

---
+++ telnyx_common/src/main/java/com/telnyx/webrtc/common/domain/authentication/AuthenticateByToken.kt
@@ -0,0 +1,23 @@
+package com.telnyx.webrtc.common.domain.authentication
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import com.telnyx.webrtc.common.TelnyxCommon
+import com.telnyx.webrtc.sdk.TokenConfig
+import com.telnyx.webrtc.sdk.model.TxServerConfiguration
+import com.telnyx.webrtc.sdk.verto.receive.ReceivedMessageBody
+import com.telnyx.webrtc.sdk.verto.receive.SocketResponse
+
+class AuthenticateByToken(private val context: Context) {
+
+    operator fun invoke(tokenConfig: TokenConfig, txPushMetaData: String? = null, autoLogin: Boolean = true): LiveData<SocketResponse<ReceivedMessageBody>> {
+        val telnyxClient = TelnyxCommon.getTelnyxClient(context)
+
+        telnyxClient.connect(TxServerConfiguration(),
+            tokenConfig,
+            txPushMetaData,
+            autoLogin)
+
+        return telnyxClient.getSocketResponse()
+    }
+}
