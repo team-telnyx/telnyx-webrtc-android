75c6fa9fc4d773f0fd6142fe4ead6992ed4de4cf
wojciechowskiradek <radoslaw@telnyx.com>
2025-05-06 12:24:54 +0200

    chore: version changed

--- samples/compose_app/build.gradle.kts
+++ samples/compose_app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.telnyx.webrtc.compose_app"
         minSdk = 27
         targetSdk = 35
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 14
+        versionName = "14"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
