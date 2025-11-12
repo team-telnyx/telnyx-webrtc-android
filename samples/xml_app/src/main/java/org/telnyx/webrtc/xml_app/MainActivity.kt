7f9b29337027a4b6cfdb1ee264d2d316a35e5b19
Oliver Zimmerman <oezimmerman@gmail.com>
2025-11-12 13:14:19 +0000

    fix: XML sample overflow menu when connected

--- samples/xml_app/src/main/java/org/telnyx/webrtc/xml_app/MainActivity.kt
+++ samples/xml_app/src/main/java/org/telnyx/webrtc/xml_app/MainActivity.kt
@@ -149,7 +149,11 @@ class MainActivity : AppCompatActivity(), DefaultLifecycleObserver {
         popupMenu.menuInflater.inflate(R.menu.overflow_menu, popupMenu.menu)
 
         val connectionStatus = currentConnectionStatus
-        val isConnected = connectionStatus != ConnectionStatus.DISCONNECTED
+        // Check connection status first, but fall back to session state if needed
+        // This handles cases where connectionStatus SharedFlow emissions were missed
+        val isConnected = (connectionStatus == ConnectionStatus.CLIENT_READY) ||
+            (connectionStatus == ConnectionStatus.DISCONNECTED &&
+             telnyxViewModel.sessionsState.value is TelnyxSessionState.ClientLoggedIn)
 
         // Hide logged-in user options when not connected
         popupMenu.menu.findItem(R.id.action_websocket_messages).isVisible = isConnected
