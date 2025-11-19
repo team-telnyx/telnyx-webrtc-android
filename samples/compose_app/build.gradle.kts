04fb14e870ae06014d4c99c5eab7bc28c5bfe5c6
Oliver Zimmerman <oezimmerman@gmail.com>
2025-11-19 18:04:08 +0000

    chore: bump app version to 19

--- samples/compose_app/build.gradle.kts
+++ samples/compose_app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "org.telnyx.webrtc.compose_app"
         minSdk = 27
         targetSdk = 35
-        versionCode = 18
-        versionName = "18"
+        versionCode = 19
+        versionName = "19"
 
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
